bbf93eb31fe1ec2f99b63d7b29454b496fa2317c
epgokce <ep.gokce@gmail.com>
2023-11-23 21:15:31 +0300

    Implemented Axios for API fetching and error

--- src/actions.js
+++ src/actions.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+import { API } from "./App";
 export const GET_FAVS_FROM_LS = "GET_FAVS_FROM_LS";
 export const FAV_ADD = "FAV_ADD";
 export const FAV_REMOVE = "FAV_REMOVE";
@@ -13,9 +15,15 @@ export const addFav = (info) => {
   return { type: FAV_ADD, payload: info }
 }
 
-export const removeFav = (id) => {
-  return { type: FAV_REMOVE, payload: id }
+export const removeFav = (activity) => {
+  return { type: FAV_REMOVE, payload: activity }
 }
 
 export const fetchAnother = () => dispatch => {
+  dispatch({ type: FETCH_LOADING });
+  axios
+    .get(API)
+    .then(res => {dispatch({ type: FETCH_SUCCESS, payload: res.data })})
+    .catch(err => dispatch({ type: FETCH_ERROR, payload: err.message }))
+    .finally(console.log("axios islemi tamamlandi."))
 }
