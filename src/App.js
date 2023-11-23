89d6f7df437e42862c749fbb56e858edf40140dd
epgokce <ep.gokce@gmail.com>
2023-11-23 20:57:00 +0300

    Added Redux functionality for favorites handling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Switch, Route, NavLink } from "react-router-dom";
 import Item from "./components/Item";
 import FavItem from "./components/FavItem";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addFav,
+  fetchAnother,
+  getFavsFromLocalStorage,
+  removeFav,
+} from "./actions";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export const API = "https://www.boredapi.com/api/activity";
 
 export default function App() {
   const loading = false;
@@ -9,7 +20,25 @@ export default function App() {
   const favs = [];
 
   function addToFavs() {
+    dispatch(addFav(current));
+    toast.success("Film favorilere eklendi", {
+      onClose: () => dispatch(fetchAnother()),
+    });
   }
+
+  function deleteFav(title) {
+    dispatch(removeFav(title));
+  }
+  const dispatch = useDispatch();
+
+  const fetchHandler = () => {
+    dispatch(fetchAnother());
+  };
+
+  useEffect(() => {
+    dispatch(fetchAnother());
+    dispatch(getFavsFromLocalStorage());
+  }, []);
 
 
   return (
@@ -39,6 +68,7 @@ export default function App() {
 
           <div className="flex gap-3 justify-end py-3">
             <button
+            onClick={fetchHandler}
               className="select-none px-4 py-2 border border-blue-700 text-blue-700 hover:border-blue-500 hover:text-blue-500"
             >
               Başka bir tane
@@ -56,7 +86,8 @@ export default function App() {
           <div className="flex flex-col gap-3">
             {favs.length > 0
               ? favs.map((item) => (
-                <FavItem key={item.key} id={item.key} title={item.activity} />
+                <FavItem key={item.key} id={item.key} activity={item.activity}
+                deleteFav={deleteFav} />
               ))
               : <div className="bg-white p-6 text-center shadow-md">Henüz bir favoriniz yok</div>
             }
