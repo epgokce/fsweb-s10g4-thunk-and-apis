4a549394ed5322e19fe4db162404b0028bebe4df
epgokce <ep.gokce@gmail.com>
2023-11-23 21:03:27 +0300

    Updated Item component to display 'activity' and 'type'

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,8 +3,9 @@ import React from 'react'
 
 function Item({ data }) {
   return (
-    <div className='shadow-md bg-white text-center'>
-      <p className='text-2xl p-10'>{data.title}</p>
+    <div className='shadow-md bg-white text-left'>
+      <p className='text-2xl p-4'><span className='text-3xl text-blue-700'>Activity: </span>{data.activity}</p>
+      <p className='text-2xl p-4'><span className='text-3xl text-blue-700'>Type: </span>{data.type}</p>
     </div>
   )
 }
